8d2dc5c64025ad3e9a4c386cf1fcf44b3c4c39a7
lightyaer <mmnyed@gmail.com>
2024-01-03 16:34:18 +0530

    add .env

--- src/app/api/edge/route.ts
+++ src/app/api/edge/route.ts
@@ -1,4 +1,4 @@
-import { db } from "@/app/db";
+import { db } from "@/db";
 import { sql } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
@@ -7,12 +7,14 @@ export const dynamicParams = true;
 export const dynamic = "force-dynamic";
 
 export const GET = async (req: NextRequest) => {
-  console.table(req.headers);
+  try {
+    const statement = sql`SELECT strftime('%Y-%m-%dT%H:%M:%S.%fZ','now')`;
+    const res = await db.get(statement);
 
-  const statement = sql`SELECT strftime('%Y-%m-%dT%H:%M:%S.%fZ','now')`;
-  const res = await db.all(statement);
+    console.log(res);
 
-  console.log(res);
-
-  return new Response(JSON.stringify(res));
+    return new Response(JSON.stringify(res));
+  } catch (e) {
+    console.log(e);
+  }
 };
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
 const client = createClient({
-    url: "./data.db",
+  url: process.env.LOCAL_URL as string,
 });
 
 export const db = drizzle(client, {
